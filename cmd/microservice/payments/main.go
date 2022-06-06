e419f8b372c180952733876022fe101d5a073474
Leksyking <gbemilekeogundipe@gmail.com>
2022-06-06 15:18:33 -0700

    add create payments microservice function

--- cmd/microservice/payments/main.go
+++ cmd/microservice/payments/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	log.Println("Starting the payments microservice")
@@ -13,4 +17,22 @@ func main() {
 	if err := paymentsInterface.Run(ctx); err != nil {
 		panic(err)
 	}
+}
+
+func createPaymentsMicroservice() {
+	cmd.WaitForService(os.Getenv("SHOP_RABBITMQ_ADDR"))
+
+	paymentsService := payments_app.NewPaymentsService(
+		payments_infra_orders.NewHTTPClient(os.Getenv("SHOP_ORDERS_SERVICE_ADDR")),
+	)
+	paymentsInterface, err := amqp.NewPaymentsInterface(
+		fmt.Sprintf("amqp://%s/", os.Getenv("SHOP_RABBITMQ_ADDR")),
+		os.Getenv("SHOP_RABBITMQ_ORDERS_TO_PAY_QUEUE"),
+		paymentsService,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	return paymentsInterface
 }
