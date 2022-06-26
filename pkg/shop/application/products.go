8fc2681eefc1076e2dcd6dee65a4cc3d8bd93be2
Leksyking <gbemilekeogundipe@gmail.com>
2022-06-26 11:25:58 -0700

    add memory.go

--- pkg/shop/application/products.go
+++ pkg/shop/application/products.go
@@ -16,12 +16,12 @@ type ProductsService struct {
 	readModel productReadModel
 }
 
-func NewProductsService() ProductsService {
-
+func NewProductsService(repo products.Repository, readModel productReadModel) ProductsService {
+	return ProductsService{repo, readModel}
 }
 
 func (s ProductsService) AllProducts() ([]products.Product, error) {
-
+	return s.readModel.AllProducts()
 }
 
 type AddProductCommand struct {
--- pkg/shop/infrastructure/products/memory.go
+++ pkg/shop/infrastructure/products/memory.go
@@ -1,16 +1,32 @@
 package products
 
 type MemoryRepository struct {
+	product []products.Product
 }
 
-func Save() {
-
+func NewMemoryRepository() *MemoryRepository {
+	return &MemoryRepository{[]products.Product{}}
+}
+func (m *MemoryRepository) Save(productToSave *products.Product) error {
+	for i, p := range m.product {
+		if p.ID() == productToSave.ID() {
+			m.product[i] = *productToSave
+			return nil
+		}
+	}
+	m.product = append(m.product, *productToSave)
+	return nil
 }
 
-func ByID() {
-
+func (m *MemoryRepository) ByID(id products.ID) (*products.Product, error) {
+	for _, p := range m.product {
+		if p.ID() == id {
+			return &p, nil
+		}
+	}
+	return nil, products.ErrNotFound
 }
 
-func AllProducts() {
-
+func (m *MemoryRepository) AllProducts() ([]products.Product, error) {
+	return m.product, nil
 }
