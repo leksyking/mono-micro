6bf1fa778b8a21b2a5a1b4cc8856e7dd8b1c0775
Leksyking <gbemilekeogundipe@gmail.com>
2022-07-02 13:02:20 -0700

    add product.go

---
+++ pkg/orders/domain/orders/product.go
@@ -0,0 +1,35 @@
+package orders
+
+import (
+	"errors"
+
+	"github.com/leksyking/monolith-microservice/pkg/common/price"
+)
+
+type ProductID string
+
+var ErrEmptyProductID = errors.New("empty product id")
+
+type Product struct {
+	id    ProductID
+	name  string
+	price price.Price
+}
+
+func NewProduct(id ProductID, name string, price price.Price) (Product, error) {
+	if len(id) == 0 {
+		return Product{}, ErrEmptyProductID
+	}
+	return Product{id, name, price}, nil
+}
+func (p Product) ID() ProductID {
+	return p.id
+}
+
+func (p Product) Name() string {
+	return p.name
+}
+
+func (p Product) Price() price.Price {
+	return p.price
+}
